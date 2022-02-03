9eddab5ce019cb326d671985ea2c50da37567365
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-02-03 16:14:22 +0200

    fix: fetchgit in super

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 self: super: {
   rofi-scripts = super.callPackage ./pkgs/rofi-scripts { };
   st = super.st.overrideAttrs ( _ : {
-    src = fetchgit {
+    src = super.fetchgit {
       url = "https://gitea.efym.net/tw1zr/st.git";
       rev= "e763f4ce3ccf6df5b462dcbf433e79fde59a444b";
       sha256="1gw7jlxh49h5r5mr6almxi59n7fwmq2szrjif9ikh32xvjxz15lc";
