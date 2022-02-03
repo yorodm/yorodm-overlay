994edddb0db773b26ecbc13190ee8472baf13197
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-02-03 17:39:50 +0200

    fix: switch st implementation

--- default.nix
+++ default.nix
@@ -3,9 +3,9 @@ self: super: {
   st = super.st.overrideAttrs (_: {
     src = super.fetchFromGitHub {
       owner = "yorodm";
-      repo = "xst";
-      rev = "d4399272c9ba405be63164bfcf4fa13b5b4d71bd";
-      sha256 = "ndBXfW57z4Piy3989nxI1sMoxeoOGCsV6Rpza5SV2RY=";
+      repo = "st";
+      rev = "3f97e54e4374ecfad2a5044367b5348896039dc3";
+      sha256 = "WCnmulP09cw/J4OBJd58yKsKZQ0bwryzK9C/4FBTK5c=";
     };
   });
 }
