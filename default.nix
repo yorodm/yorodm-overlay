6c751f7c98c5aed514241cc0dbdf87aaf18a7aeb
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-02-03 18:02:27 +0200

    fix: switch to another st

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 self: super: {
   rofi-scripts = super.callPackage ./pkgs/rofi-scripts { };
-  st = super.st.overrideAttrs (_: {
-    src = super.fetchFromGitHub {
+  st = super.st.overrideAttrs (old: {
+    # I don't know is this is the right way
+    buildInputs = old.buildInputs ++ [ super.cairo ];
+    src = pkgs.fetchFromGitHub {
       owner = "yorodm";
       repo = "st";
       rev = "3f97e54e4374ecfad2a5044367b5348896039dc3";
