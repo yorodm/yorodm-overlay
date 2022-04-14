4e465834db7ed13ccba894c490aa58bf6cb53db6
Yoandy Rodriguez <yoandy.rmartinez@gmail.com>
2022-04-15 01:13:12 +0300

    2022-04-15T01:13:22+03:00

--- pkgs/rofi-scripts/rofi-power.nix
+++ pkgs/rofi-scripts/rofi-power.nix
@@ -1,40 +1,39 @@
-{symlinkJoin, gawk, bash, writeShellScriptBin, rofi, betterlockscreen}:
+{ symlinkJoin, gawk, bash, writeShellScriptBin, rofi, betterlockscreen, theme ?
+  "listview { layout:horizontal;} window {width:20%; children:[horibox];} horibox {children:[listview];}"
+}:
 
 let
   awk = "${gawk}/bin/awk";
   rofi-power = writeShellScriptBin "rofi-power" ''
-  #!${bash}/bin/bash
-  enumerate () {
-    ${awk} -F"|"  '{ for (i = 1; i <= NF; ++i) print "<big>"$i"</big><sub><small>"i"</small></sub>"; exit } '
-  }
-  question=$(printf "||||"| enumerate|rofi -dmenu -markup-rows)
+    #!${bash}/bin/bash
+    enumerate () {
+      ${awk} -F"|"  '{ for (i = 1; i <= NF; ++i) print "<big>"$i"</big><sub><small>"i"</small></sub>"; exit } '
+    }
+    question=$(printf "||||"| enumerate|rofi -dmenu -markup-rows "${theme}")
 
-  case $question in
-    **)
-        ${betterlockscreen}/bin/betterlockscreen --lock
-        ;;
-    **)
-        systemctl suspend
-        ;;
-    **)
-        loginctl terminate-session $XDG_SESSION_ID
-        ;;
-    **)
-        systemctl reboot
-        ;;
-    **)
-        systemctl poweroff
-        ;;
-    *)
-        exit 0  # do nothing on wrong response
-        ;;
-  esac
+    case $question in
+      **)
+          ${betterlockscreen}/bin/betterlockscreen --lock
+          ;;
+      **)
+          systemctl suspend
+          ;;
+      **)
+          loginctl terminate-session $XDG_SESSION_ID
+          ;;
+      **)
+          systemctl reboot
+          ;;
+      **)
+          systemctl poweroff
+          ;;
+      *)
+          exit 0  # do nothing on wrong response
+          ;;
+    esac
   '';
 
-in
-symlinkJoin  {
+in symlinkJoin {
   name = "rofi-scripts";
-  paths = [
-    rofi-power
-  ];
+  paths = [ rofi-power ];
 }
